299db6de779e4e43c2daeb171052b6c7886c5c7c
ZDmitry <hiroshidi@gmail.com>
2016-12-23 20:18:19 +0200

    added parsing key space notifications

--- index.js
+++ index.js
@@ -16,12 +16,40 @@ let call_notification_listeners = function (notification) {
   for (i; i >= 0; i--) {
     notification_listeners[i](notification);
   }
-}
+};
+
+let keySpaceNotificationTransform = function(msg) {
+  if (msg) {
+    let eventName = msg.event || '';
+    if (eventName.startsWith('__keyspace@')) {
+      eventName   = eventName.slice('__keyspace@'.length);
+
+      let dbName  = eventName.split(':')[0];
+      let varName = eventName.split(':')[1];
+
+      dbName = dbName[0];
+
+      return {
+        action: msg.message,
+        target: varName,
+        scope:  parseInt(dbName)
+      }
+    }
+  }
+};
 
 switch (Platform.OS) {
   case 'android':
     bridge = ReactNativeRedisAndroid;
     DeviceEventEmitter.addListener('redis.event', (notification) => {
+      notification = (notification && notification.result);
+
+      let keySpaceMsg = keySpaceNotificationTransform(notification);
+
+      if (keySpaceMsg) {
+        notification = keySpaceMsg
+      }
+
       call_notification_listeners(notification);
     });
     break;
